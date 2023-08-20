603a993363e7c62690a8a9a5a0f3fefb4356cfb7
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-08-20 18:22:36 -0300

    add plataformas

--- sketch.js
+++ sketch.js
@@ -1,12 +1,18 @@
 
 let night_sky;
 let chao;
+let nuvens;
+let tamanho = 64;
+let blocos = [];
+let blocoImg;
 
 
 
 function preload() {
     night_sky = loadImage('imagens/night-sky.png');
     chao = loadImage('imagens/chao.png');
+    nuvens = loadImage('imagens/nuvens.png');
+    blocoImg = loadImage('imagens/bloco.png');
 }
 
 function setup() {
@@ -14,14 +20,47 @@ function setup() {
     rectMode(CENTER);
     textAlign(CENTER);
     //imageMode(CENTER);
+
+    blocos = [
+        { x: 120, y: 232 },
+        { x: 184, y: 232 },
+        { x: 248, y: 232 },
+        { x: 312, y: 232 },
+
+        { x: 520, y: 332 },
+        { x: 584, y: 332 },
+        { x: 648, y: 332 },
+        { x: 712, y: 332 }
+    ]
 }
 
 function draw() {
     background(2, 96, 188);
-    image(night_sky, 0, 0, 929, 450); //541, 208
-    image(chao, 0, 330, 929, 250);
-    fill(0);
-    rect(811, 520, 235, 150);
+    //fundo ceu
+    image(night_sky, 0, 0, width, height); //541, 208 
+
+    //fundo nuvens e montanhas
+    image(nuvens, 0, 0, width, height - 65);
+
+    // chao
+    for (let i = 0; i < 15; i++) {
+        image(chao, tamanho * i, tamanho * 8, tamanho, tamanho);
+    }
+
+    // blocos amarelos
+    for (let i = 0; i < blocos.length; i++) {
+        image(blocoImg, blocos[i].x, blocos[i].y, tamanho, tamanho);
+    }
+
+
+
+    //borda tela
+    stroke(10);
+    strokeWeight(10);
+    noFill();
+    rect(width / 2, height / 2, width - 10, height - 10);
+
 
 
 }
+
